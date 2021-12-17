d018a4c1d7ed0e1e690d42fb317a67b288c04f87
psp1230 <31025050+psp1230@users.noreply.github.com>
2021-12-17 09:47:33 +0800

    [feat]vite.config路徑配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,18 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@/': path.resolve(__dirname, './src'),
-      '@/config': path.resolve(__dirname, './src/config'),
-      '@/components': path.resolve(__dirname, './src/components'),
-      '@/styles': path.resolve(__dirname, './src/styles'),
-      '@/utils': path.resolve(__dirname, './src/utils'),
-      '@/common': path.resolve(__dirname, './src/common'),
-      '@/assets': path.resolve(__dirname, './src/assets'),
-      '@/pages': path.resolve(__dirname, './src/pages'),
-      '@/routes': path.resolve(__dirname, './src/router'),
-      '@/layouts': path.resolve(__dirname, './src/layouts'),
-      '@/hooks': path.resolve(__dirname, './src/hooks'),
-      '@/store': path.resolve(__dirname, './src/store')
+      '@': path.resolve(__dirname, 'src'),
+      '@/config': path.resolve(__dirname, 'src/config'),
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/styles': path.resolve(__dirname, 'src/styles'),
+      '@/utils': path.resolve(__dirname, 'src/utils'),
+      '@/common': path.resolve(__dirname, 'src/common'),
+      '@/assets': path.resolve(__dirname, 'src/assets'),
+      '@/pages': path.resolve(__dirname, 'src/pages'),
+      '@/routes': path.resolve(__dirname, 'src/router'),
+      '@/layouts': path.resolve(__dirname, 'src/layouts'),
+      '@/hooks': path.resolve(__dirname, 'src/hooks'),
+      '@/store': path.resolve(__dirname, 'src/store')
     }
   },
 })
