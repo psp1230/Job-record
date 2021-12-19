c3e62619a01e3c8a2be22c9a18d35a262685ae4d
psp1230 <31025050+psp1230@users.noreply.github.com>
2021-12-19 16:32:06 +0800

    [feat]基本資料性別比率

---
+++ src/components/GenderRate.tsx
@@ -0,0 +1,51 @@
+import ReactECharts from 'echarts-for-react';
+
+function GenderRate(): JSX.Element {
+  const option = {
+    title: {
+      text: '性別總佔比',
+      left: 'left'
+    },
+    tooltip: {
+      show: false,
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'horizontal',
+      bottom: 0,
+      right: 0,
+      lineStyle: {
+        width: 0
+      },
+      icon: 'circle'
+    },
+    series: [
+      {
+        type: 'pie',
+        radius: '70%',
+        data: [
+          { value: 1048, name: '男生', itemStyle: { color: '#448AFF' } },
+          { value: 735, name: '女生', itemStyle: { color: '#FF5252' } },
+        ],
+        markLine: {
+          symbol: 'none'
+        },
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+  return (
+    <div className="w-72 p-6">
+      <ReactECharts option={option} />
+    </div>
+  )
+}
+
+export default GenderRate
--- src/pages/Firstjob.tsx
+++ src/pages/Firstjob.tsx
@@ -1,45 +1,15 @@
-// import { useState } from 'react'
-// import logo from '@/logo.svg'
-// import '@/App.css'
+import GenderRate from '@/components/GenderRate';
 
 function FirstJob(): JSX.Element {
-  // const [count, setCount] = useState(0)
 
   return (
     <div className="flex-grow">
-      這裡是第一份工作
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-300 text-[60px]">Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header> */}
+    <div className="flex justify-between">
+      <div className="border border-gray-400">
+        <GenderRate />
+      </div>
     </div>
+  </div>
   )
 }
 
